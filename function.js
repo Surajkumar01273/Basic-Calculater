0e270bb0ee2cd05b65a7aaeca4ae3a696c383bc8
suraj kumar <surajshk1995@gmail.com>
2024-05-13 20:47:06 +0530

    version 1.0

---
+++ function.js
@@ -0,0 +1,31 @@
+    let a,b,c;
+
+    function caladd(firstnumber,secondnumber){
+        a = parseInt(firstnumber);
+        b = parseInt(secondnumber);
+        c = a+b;
+        alert("Result is =" +c);
+
+    }
+    function calsub(firstnumber,secondnumber){
+        a = parseInt(firstnumber);
+        b = parseInt(secondnumber);
+        c = a-b;
+        alert("Result is =" + c);
+    }
+    function calmulti(firstnumber,secondnumber){
+        a = parseInt(firstnumber);
+        b = parseInt(secondnumber);
+        c = a*b;
+        alert("Result is ="+ c);
+    }
+    function caldiv(firstnumber,secondnumber){
+        a = parseInt(firstnumber);
+        b = parseInt(secondnumber);
+        c = a/b;
+        alert("Result is =" + c);
+    }
+    
+
+
+
